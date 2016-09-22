13fe11493dd44308984d5bb63147d2e25e0896a3
nma2207 <nma2207@gmail.com>
2016-09-22 13:35:09 +0300

    second commit

--- 19_09_16_1.r
+++ 19_09_16_1.r
@@ -1,9 +1,9 @@
-x=read.csv(file="D:/учеба/пзтв/7_R1_Z1.csv",h=T,dec=".")$x
-source("D:/учеба/пзтв/functions.r")
+п»їx=read.csv(file="D:/СѓС‡РµР±Р°/РїР·С‚РІ/7_R1_Z1.csv",h=T,dec=".")$x
+source("D:/СѓС‡РµР±Р°/РїР·С‚РІ/functions.r")
 results<-c(length(x), mean(x), median(x), dispersia(x), var(x), sqrt(dispersia(x)), min(x), max(x), max(x)-min(x), assymetrya(x), ekscess(x))
-names(results)<-c("Объем выборки         ", "Среднее               ", "Медиана               ", "Смещенная дисперсия   ", "Несмещенная дисперсия ", "Стандартное отклонение",
-"Минимум               ", "Максимум              ", "Размах                ", "Ассиметрия            ", "Эксцесс               ")
+names(results)<-c("РћР±СЉРµРј РІС‹Р±РѕСЂРєРё         ", "РЎСЂРµРґРЅРµРµ               ", "РњРµРґРёР°РЅР°               ", "РЎРјРµС‰РµРЅРЅР°СЏ РґРёСЃРїРµСЂСЃРёСЏ   ", "РќРµСЃРјРµС‰РµРЅРЅР°СЏ РґРёСЃРїРµСЂСЃРёСЏ ", "РЎС‚Р°РЅРґР°СЂС‚РЅРѕРµ РѕС‚РєР»РѕРЅРµРЅРёРµ",
+"РњРёРЅРёРјСѓРј               ", "РњР°РєСЃРёРјСѓРј              ", "Р Р°Р·РјР°С…                ", "РђСЃСЃРёРјРµС‚СЂРёСЏ            ", "Р­РєСЃС†РµСЃСЃ               ")
 d=data.frame(result=results)
-write.table(d, file = "D:/учеба/пзтв/Z1.txt", sep = "     ", col.names =NA, quote=F)
+write.table(d, file = "D:/СѓС‡РµР±Р°/РїР·С‚РІ/Z1.txt", sep = "     ", col.names =NA, quote=F)
 hist(x, breaks=10)
 d
